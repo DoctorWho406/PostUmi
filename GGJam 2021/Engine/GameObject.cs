57d51c5e2b17c67a935d9c3aa219ee5c8ebeaa51
Jumbax <jumbax94@gmail.com>
2021-02-16 18:37:06 +0100

    GlitchableObject

--- GGJam 2021/Engine/GameObject.cs
+++ GGJam 2021/Engine/GameObject.cs
@@ -16,8 +16,6 @@ namespace GGJam_2021 {
             get => sprite.position;
             set {
                 sprite.position = value;
-                spriteGlitch1.position = value;
-                spriteGlitch2.position = value;
             }
         }
         public LayerMask LayerMask;
@@ -31,10 +29,7 @@ namespace GGJam_2021 {
         protected Vector2 halfSize {
             get; private set;
         }
-        protected bool glitch;
-        private float timer;
-        protected bool glithched;
-
+        
         public GameObject(string textureName, LayerMask layerMask, int w = 0, int h = 0, float fps = 0) {
             //Set LayerMask
             LayerMask = layerMask;
@@ -47,15 +42,10 @@ namespace GGJam_2021 {
             textureOffset = Vector2.Zero;
 
             sprite = new Sprite(w, h);
-            spriteGlitch1 = new Sprite(w, h);
-            spriteGlitch2 = new Sprite(w, h);
-
-            spriteGlitch1.SetMultiplyTint(Constants.tintaBlue);
-            spriteGlitch2.SetMultiplyTint(Constants.tintaGialla);
+            
 
             sprite.pivot = new Vector2(sprite.Width * 0.5f, sprite.Height * 0.5f);
-            spriteGlitch1.pivot = sprite.pivot;
-            spriteGlitch2.pivot = sprite.pivot;
+            
 
             Size = new Vector2(sprite.Width, sprite.Height);
             halfSize = new Vector2(Size.X * 0.5f, Size.Y * 0.5f);
@@ -63,41 +53,18 @@ namespace GGJam_2021 {
 
         public virtual void Scale(float scaleFactory) {
             sprite.scale = new Vector2(scaleFactory);
-            spriteGlitch1.scale = new Vector2(scaleFactory);
-            spriteGlitch2.scale = new Vector2(scaleFactory);
             Size *= scaleFactory;
             halfSize *= scaleFactory;
         }
 
-        public virtual void SetGlitch(bool value) {
-            glitch = value;
-        }
 
-        public bool GetGlitch() {
-            return glitch;
-        }
 
-        public virtual void Update() {
-            if (glitch) {
-                timer += Game.DeltaTime;
-                if (timer > Constants.GlitchTime) {
-                    timer = 0f;
-                    glithched = !glithched;
-                }
-            }
-        }
-
-        public virtual void Draw() {
-            if (!glitch) {
-                sprite.DrawTexture(texture, (int)textureOffset.X, (int)textureOffset.Y, (int)sprite.Width, (int)sprite.Height);
-            } else {
-                if (glithched) {
-                    spriteGlitch1.DrawTexture(texture, (int)textureOffset.X, (int)textureOffset.Y, (int)sprite.Width, (int)sprite.Height);
-                } else {
-                    spriteGlitch2.DrawTexture(texture, (int)textureOffset.X, (int)textureOffset.Y, (int)sprite.Width, (int)sprite.Height);
-                }
-            }
-        }
+        public virtual void Draw()
+        {
+            spriteGlitch2.DrawTexture(texture, (int)textureOffset.X, (int)textureOffset.Y, (int)sprite.Width, (int)sprite.Height);
+        }  
+            
+            
 
         public virtual void OnCollide(GameObject other) {
 
---
+++ GGJam 2021/GameObject/GlitchableObject.cs
@@ -0,0 +1,82 @@
+﻿using Aiv.Fast2D;
+using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GGJam_2021
+{
+    class GlitchableObject : GameObject
+    {
+        public override Vector2 Position
+        {
+            get => sprite.position;
+            set
+            {
+                spriteGlitch1.position = value;
+                spriteGlitch2.position = value;
+            }
+        }
+        protected bool glitch;
+        private float timer;
+        protected bool glithched;
+
+        public GlitchableObject(string textureName, LayerMask layerMask, int w = 0, int h = 0, float fps = 0) : base(textureName, layerMask, w, h, fps)
+        {
+            spriteGlitch1 = new Sprite(w, h);
+            spriteGlitch2 = new Sprite(w, h);
+
+            spriteGlitch1.SetMultiplyTint(Constants.tintaBlue);
+            spriteGlitch2.SetMultiplyTint(Constants.tintaGialla);
+
+            spriteGlitch1.pivot = sprite.pivot;
+            spriteGlitch2.pivot = sprite.pivot;
+        }
+
+        public override void Scale(float scaleFactory)
+        {
+            spriteGlitch1.scale = new Vector2(scaleFactory);
+            spriteGlitch2.scale = new Vector2(scaleFactory);
+        }
+
+        public virtual void SetGlitch(bool value)
+        {
+            glitch = value;
+        }
+
+        public bool GetGlitch()
+        {
+            return glitch;
+        }
+
+        public virtual void Update()
+        {
+            if (glitch)
+            {
+                timer += Game.DeltaTime;
+                if (timer > Constants.GlitchTime)
+                {
+                    timer = 0f;
+                    glithched = !glithched;
+                }
+            }
+        }
+
+        public override void Draw()
+        {
+            if (!glitch)
+            {
+                sprite.DrawTexture(texture, (int)textureOffset.X, (int)textureOffset.Y, (int)sprite.Width, (int)sprite.Height);
+            }
+            else
+            {
+                if (glithched)
+                {
+                    spriteGlitch1.DrawTexture(texture, (int)textureOffset.X, (int)textureOffset.Y, (int)sprite.Width, (int)sprite.Height);
+                }
+            }
+        }
+    }
+}
